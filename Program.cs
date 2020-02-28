bc7315489f34808aef6710e9b3a8c8dac678f5d3
AMBULATUR <ambulatur@gmail.com>
2020-02-28 23:34:00 +0300

    Upload

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Server
+    {
+        TcpListener Listener;
+        public Server(int Port)
+        {
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start();
+            while (true)
+            {
+                Task.Delay(1000);
+                new Client(Listener.AcceptTcpClient());
+            }
+        }
+        // StopServer
+        ~Server()
+        {
+            // If listener created
+            if (Listener != null)
+            {
+                Listener.Stop();
+            }
+        }
+    }
+    class Client
+    {
+        public Client(TcpClient Client)
+        {
+            string Html = "<html><body><h1>Heroku SocketServer</h1></body></html>";
+            string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            Client.Close();
+        }
+    }
+
+    class Program
+    {
+ 
+        static async void StartServerAsync()
+        {
+            Console.WriteLine("Boot server");
+            var port = Environment.GetEnvironmentVariable("PORT");
+            Console.WriteLine(Environment.GetEnvironmentVariable("CNAME"));
+            Console.WriteLine("Port " + port);
+            await Task.Run(() => new Server(Convert.ToInt32(port)));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Automatic Deploy");
+
+            try
+            {
+                StartServerAsync();
+              //  SendMessageFromSocket(11000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void SendMessageFromSocket(int port)
+        {
+            // Буфер для входящих данных
+            byte[] bytes = new byte[1024];
+
+            // Соединяемся с удаленным устройством
+
+            // Устанавливаем удаленную точку для сокета
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Соединяем сокет с удаленной точкой
+            sender.Connect(ipEndPoint);
+
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("Сокет соединяется с {0} ", sender.RemoteEndPoint.ToString());
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+
+            // Отправляем данные через сокет
+            int bytesSent = sender.Send(msg);
+
+            // Получаем ответ от сервера
+            int bytesRec = sender.Receive(bytes);
+
+            Console.WriteLine("\nОтвет от сервера: {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+            // Используем рекурсию для неоднократного вызова SendMessageFromSocket()
+            if (message.IndexOf("<TheEnd>") == -1)
+                SendMessageFromSocket(port);
+
+            // Освобождаем сокет
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
